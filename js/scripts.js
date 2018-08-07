fbcc8cb12cce416280e991179a8f1b25e6ca6763
Vik <kvvendetta@gmail.com>
2018-08-07 16:26:02 +0300

    Wrote the functionality for tabs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
  $( function() {
     $( "#tabs" ).tabs();
+
+    var themesItems = $(".themes-item");
+
+    themesItems.on("click", function() {
+    	$(this).addClass("active").siblings().removeClass("active");
+    })
  });
